1964d1ffcc2dc951c91bfaaec6998491186234dd
dronewizard <aylstockjon@gmail.com>
2018-05-20 20:44:07 -0500

    naming in RPi folder and add relay switching script for pin 7

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/push.sh
 # Quick way to commit and push
-comment=$update naming in RPi folder and add relay switching script for pin 7
-echo Commit comment: \"$update naming in RPi folder and add relay switching script for pin 7\"
+comment=$ update further naming revisions in RPi
+echo Commit comment: \"$ update further naming revisions in RPi\"
 # upload in this folder
 
 echo "============ Uploading to GitHub ============"
